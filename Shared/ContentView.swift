0576432af4703c52c795c581071b06e922973a1d
Art Huang <arth@verizonmedia.com>
2020-07-17 10:32:27 +0800

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Art Huang on 2020/7/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/GenericBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenericBugApp.swift
+//  Shared
+//
+//  Created by Art Huang on 2020/7/17.
+//
+
+import SwiftUI
+
+@main
+struct GenericBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
